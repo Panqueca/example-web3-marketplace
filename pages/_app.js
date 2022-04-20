1da0f2f129f05a1f81f07e69d6d953367d58d961
Panqueca <reyrogerio@hotmail.com>
2022-04-20 15:23:51 -0300

    Added UserContext to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,38 @@
-import Head from 'next/Head'
+import { useState, useEffect } from 'react'
 import { ThemeProvider } from 'theme-ui'
 import theme from './theme'
-import Meta from './components/Meta'
 import '../styles/globals.css'
 import '../styles/products.css'
+import { UserContext } from './context/user'
 
 function MyApp({ Component, pageProps }) {
+  const [user, setUser] = useState({ isConnected: false })
+
+  function updateUserField(field, newValue) {
+    setUser(current => {
+      return {
+        ...current,
+        [field]: newValue,
+      }
+    })
+  }
+
+  function disconnectAccount() {
+    updateUserField('isConnected', false)
+    updateUserField('address', '')
+  }
+
+  useEffect(() => {
+    setUser({ ...user, updateUserField, disconnectAccount })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <UserContext.Provider value={user}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </UserContext.Provider>
   )
 }
 
